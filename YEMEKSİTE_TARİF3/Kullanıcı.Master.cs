1fe97e6cbedd5d22e29beb5f66bb53833e02969e
RYDEAON <116547078+RYDEAON@users.noreply.github.com>
2024-04-06 16:09:57 +0300

    7.ders

--- YEMEKSİTE_TARİF3/Kullanıcı.Master.cs
+++ YEMEKSİTE_TARİF3/Kullanıcı.Master.cs
@@ -4,14 +4,19 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Data.SqlClient;
 
 namespace YEMEKSİTE_TARİF3
 {
 	public partial class Kullanıcı : System.Web.UI.MasterPage
 	{
+		sqlsinif bgl=new sqlsinif();
 		protected void Page_Load(object sender, EventArgs e)
 		{
-
+			SqlCommand komut = new SqlCommand("Select * From Tbl_Kategoriler",bgl.baglanti());
+			SqlDataReader oku = komut.ExecuteReader();
+			DataList1.DataSource = oku;
+			DataList1.DataBind();
 		}
 	}
 }
