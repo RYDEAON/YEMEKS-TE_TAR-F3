ac2a9bf5f370a1aebe8c12dcd143fff84c0aa53a
RYDEAON <116547078+RYDEAON@users.noreply.github.com>
2024-04-06 15:32:47 +0300

    6.DERS

---
+++ YEMEKSİTE_TARİF3/sqlsinif.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.SqlClient;
+
+namespace YEMEKSİTE_TARİF3
+{
+	public class sqlsinif
+	{
+		public SqlConnection baglanti()
+		{
+			SqlConnection baglan = new SqlConnection(@"Data Source=DESKTOP-UNAPEKC\SQLEXPRESS;Initial Catalog=Dbo_yemektarifi;Integrated Security=True;Encrypt=False;");
+            baglan.Open();
+			return baglan;
+	}
+	} 
+}
+	
